8aed4779c19d7d97424cf9d5c49fb5ef86ecf248
Paulo Sergio <paulosxb@gmail.com>
2023-10-25 16:33:38 -0300

    Formulas de matematica
    1 - Escreva um programa que solicite ao usuario dois numeros e exiba a soma, subtração, multiplicação e divisão entre eles

---
+++ calculoSomaDivMultSub.util.java
@@ -0,0 +1,26 @@
+/*1 - Escreva um programa que solicite ao usuario dois numeros e exiba a soma, subtração, multiplicação e divisão entre eles*/
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nexDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nexDouble();
+
+        double soma - numero1 + numero2;
+        double subtracao = numero1 + numero2;
+        double multiplicacao = numero1 * numero2;
+        double divisao = numero1 / numero2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+
+        scanner.close();
+}
